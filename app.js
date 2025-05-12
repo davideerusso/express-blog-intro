6cf51f6952ebe904b6966854f79848372ae8cf58
davideerusso <rd.96.rd@gmail.com>
2025-05-12 16:55:42 +0200

    posts

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { posts } = require("./post");
+
 app.use(express.static("public"));
 
 app.listen(port, () => {
@@ -10,41 +12,8 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send("Server del mio blog");
-  res.json(posts);
 });
 
 app.get("/bacheca", (req, res) => {
-  const posts = [
-    {
-      Titolo: "luogo 1",
-      Contenuto: "...",
-      img: "/image/ciambellone.jpeg",
-      tags: ["mare", "montagna", "lago"],
-    },
-    {
-      Titolo: "luogo 2",
-      Contenuto: "...",
-      img: "/image/cracker_barbabietola.jpeg",
-      tags: ["mare", "montagna", "lago"],
-    },
-    {
-      Titolo: "luogo 3",
-      Contenuto: "...",
-      img: "/image/pane_fritto_dolce.jpeg",
-      tags: ["mare", "montagna", "lago"],
-    },
-    {
-      Titolo: "luogo 4",
-      Contenuto: "...",
-      img: "/image/pasta_barbabietola.jpeg",
-      tags: ["mare", "montagna", "lago"],
-    },
-    {
-      Titolo: "luogo 5",
-      Contenuto: "...",
-      img: "/torta_paesana.jpeg",
-      tags: ["mare", "montagna", "lago"],
-    },
-    res.json(posts),
-  ];
+  res.json(posts);
 });
---
+++ post.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    Titolo: "luogo 1",
+    Contenuto: "...",
+    img: "/image/ciambellone.jpeg",
+    tags: ["mare", "montagna", "lago"],
+  },
+  {
+    Titolo: "luogo 2",
+    Contenuto: "...",
+    img: "/image/cracker_barbabietola.jpeg",
+    tags: ["mare", "montagna", "lago"],
+  },
+  {
+    Titolo: "luogo 3",
+    Contenuto: "...",
+    img: "/image/pane_fritto_dolce.jpeg",
+    tags: ["mare", "montagna", "lago"],
+  },
+  {
+    Titolo: "luogo 4",
+    Contenuto: "...",
+    img: "/image/pasta_barbabietola.jpeg",
+    tags: ["mare", "montagna", "lago"],
+  },
+  {
+    Titolo: "luogo 5",
+    Contenuto: "...",
+    img: "/torta_paesana.jpeg",
+    tags: ["mare", "montagna", "lago"],
+  },
+];
+
+module.exports = { posts };
